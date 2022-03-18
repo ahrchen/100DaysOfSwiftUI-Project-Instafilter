9d2aeffb8bef306bba0e47c98c1bc1c866577784
Raymond Chen <ahrchen@gmail.com>
2022-03-18 13:32:38 -0700

    Initial Commit

---
+++ Instafilter/InstafilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstafilterApp.swift
+//  Instafilter
+//
+//  Created by Raymond Chen on 3/18/22.
+//
+
+import SwiftUI
+
+@main
+struct InstafilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
